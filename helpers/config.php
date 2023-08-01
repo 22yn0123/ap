de154d1edfe94aaf97fef208fad832c3355e7c84
unknown <22yn0123@JN01.JEC.AC.JP>
2023-08-01 20:14:33 +0900

    DB接続文字列を732実習室からAzureSQLデータベースへ変更

--- helpers/config.php
+++ helpers/config.php
@@ -1,5 +1,5 @@
 <?php
 // DB接続設定
-define('DSN', "sqlsrv:server = tcp:22yn0123db.database.windows.net,1433; Database = 22yn0123", "jndb", "{your_password_here}");
-define('DB_USER', '22yn0123');
-define('DB_PASSWORD', '22yn0123');
+define('DSN', 'sqlsrv:server = tcp:22yn0123db.database.windows.net,1433; Database = 22yn0123');
+define('DB_USER', 'jndb');
+define('DB_PASSWORD', 'Pa$$word1234');
